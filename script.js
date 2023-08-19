f0831fd1196743176f40a9922725be18cf89181e
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 02:01:20 -0700

    capitalize-letters assignment done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+const sentence = "my name is rayyan";
+const sentence = "my name is rayyan";
+const words = mySentence.split(" ");
+
+const sentence = "my name is rayyan";
+const words = sentence.split(" ");
+
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+
+const sentence = "my name is rayyn";
+const words = sentence.split(" ");
+
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+
+words.join(" ");
+
